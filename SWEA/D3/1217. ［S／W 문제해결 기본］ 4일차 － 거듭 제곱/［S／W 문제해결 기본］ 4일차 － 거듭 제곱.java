2c588473090260638cbebb2e2664ddf27e989e16
WJ <130027416+OiPKL@users.noreply.github.com>
2024-08-28 13:32:45 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 145 ms, Memory: 21,372 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,27 +1,36 @@
 import java.util.Scanner;
 
 public class Solution {
+	
+	static int N;
+	static int M;
+	static int num;
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		for (int t = 1; t <= 10; t++) {
+		for (int tc = 1; tc <= 10; tc++) {
 			
-			int T = sc.nextInt();
+			int TC = sc.nextInt();
+			N = sc.nextInt();
+			M = sc.nextInt();
+			num = 1;
 			
-			int M = sc.nextInt();
-			int N = sc.nextInt();
+			zgop(false, 0);
 			
-			int result = zegop(M, N);
-			
-			System.out.println("#" + t + " " + result);
+			System.out.println("#" + TC + " " + num);
+		
 		}
+		
 	}
-	        		
-	static int zegop (int M, int N) {
-		if (N == 0)
-			return 1;
-		else
-			return M * zegop(M, N-1);
+	
+	static void zgop(boolean isComplete, int cnt) {
+		if (isComplete) return;
+		if (cnt == M - 1) isComplete = true;
+
+		num *= N;
+		zgop(isComplete, cnt + 1);
 	}
+	
 }
