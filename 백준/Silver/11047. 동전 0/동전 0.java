c272dd9ca90f4c222f6555a229c8ca87a3ef100a
WJ <130027416+OiPKL@users.noreply.github.com>
2024-09-27 07:51:21 +0900

    [Silver IV] Title: 동전 0, Time: 164 ms, Memory: 13060 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-
+		
 		int N = sc.nextInt();
 		int K = sc.nextInt();
+		
+		int[] coins = new int[N+1];
+		for (int n = 1; n <= N; n++)
+			coins[n] = sc.nextInt();
+		
 		int cnt = 0;
-		
-		int[] bank = new int[N];
-		
-		for (int i = 0; i < N; i++) {
-			bank[i] = sc.nextInt();
+		for (int n = N; n >= 1; n--) {
+			if (K >= coins[n])
+				while (K >= coins[n]) {
+					K -= coins[n];
+					cnt++;
+				}
 		}
-
-		for (int i = N-1; i >= 0; i--) {
-			int tmp = K / bank[i];
-			K %= bank[i];
-			cnt += tmp;
-			if (K == 0) break;
-		}
+		
 		System.out.println(cnt);
-		
 	}
 }
